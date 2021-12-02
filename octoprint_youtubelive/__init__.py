9747a33e2bb31af2e5d42c6b5de176bc174c7eff
Moises Martinez <moimart@gmail.com>
2021-12-02 10:01:15 +0100

    changes

--- octoprint_youtubelive/__init__.py
+++ octoprint_youtubelive/__init__.py
@@ -97,8 +97,8 @@ class youtubelive(octoprint.plugin.StartupPlugin,
 			try:
 				self.container = self.client.containers.run("octoprint/youtubelive:latest",command=[self._settings.get(["webcam_url"]),self._settings.get(["stream_id"]),"null","15"],detach=True,privileged=False,devices=["/dev/vchiq"],name="YouTubeLive",auto_remove=True,network_mode="host")
 				self._plugin_manager.send_plugin_message(self._identifier, dict(status=True,streaming=True))
-				self._logger.info(self._settings.get(["webcam_url"]))
-				self._logger.info(self.container.logs())
+				self._logger.info("Framerate: {}".format(self._settings.get(["framerate"])))
+				#self._logger.info(self.container.logs())
 			except Exception as e:
 				self._plugin_manager.send_plugin_message(self._identifier, dict(error=str(e),status=True,streaming=False))
 		return
